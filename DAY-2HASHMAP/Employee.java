3a3f7c8c395eed3347995b57c02597eb0ac53c9c
pranavaneechadi <pranavneechadi@gmail.com>
2026-08-06 20:09:44 +0530

    second-commit

---
+++ DAY-2HASHMAP/Employee.java
@@ -0,0 +1,39 @@
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+    private String department;
+
+    public Employee(int id, String name, double salary, String department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + salary + " " + department;
+    }
+}
